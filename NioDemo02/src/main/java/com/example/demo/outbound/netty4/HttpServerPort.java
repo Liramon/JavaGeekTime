1a8d15677e3a8bb60f448db7bd2ccfcf79c003db
liramon <liramon@aliyun.com>
2021-05-23 21:06:13 +0800

    initNio02

---
+++ NioDemo02/src/main/java/com/example/demo/outbound/netty4/HttpServerPort.java
@@ -0,0 +1,45 @@
+package com.example.demo.outbound.netty4;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+// 创建了一个固定大小的线程池处理请求
+public class HttpServerPort {
+    public static void main(String[] args) throws IOException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(
+                Runtime.getRuntime().availableProcessors() + 2);
+        final ServerSocket serverSocket = new ServerSocket(8801);
+        while (true) {
+            try {
+                final String body = "hello,port::::::::::" + serverSocket.getLocalPort();
+                final Socket socket = serverSocket.accept();
+                executorService.execute(() -> service(socket, body));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void service(Socket socket, String body) {
+        try {
+//            Thread.sleep(5);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            printWriter.println("Content-Length:" + body.getBytes().length);
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.close();
+            socket.close();
+        } catch (IOException e) { // | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ NioDemo02/src/main/java/com/example/demo/outbound/okhttp/OkhttpOutboundHandler.java
@@ -0,0 +1,4 @@
+package com.example.demo.outbound.okhttp;
+
+public class OkhttpOutboundHandler {
+}
