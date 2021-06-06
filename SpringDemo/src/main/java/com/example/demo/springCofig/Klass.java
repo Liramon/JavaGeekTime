f892bdd9d45f3a4daccf737ef05d00ebb275c044
liramon <liramon@aliyun.com>
2021-06-06 20:38:46 +0800

    initSpring

---
+++ SpringDemo/src/main/java/com/example/demo/springCofig/Klass.java
@@ -0,0 +1,17 @@
+package com.example.demo.springCofig;
+
+import com.example.demo.springCofig.Student;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Klass { 
+    
+    List<Student> students;
+    
+    public void dong(){
+        System.out.println(this.getStudents());
+    }
+    
+}
